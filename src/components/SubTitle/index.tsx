98f71db93a38d8dce858b52ffc3d11925e81613e
Darius K <darius.kaya@gmail.com>
2024-09-13 12:37:14 +0200

    subtitle unit test pass

--- src/components/SubTitle/index.tsx
+++ src/components/SubTitle/index.tsx
@@ -1,11 +1,11 @@
 
 type SubTitleProps = {
-    heroMsg: string
+    title: string
 }
 
-const SubTitle = ({ heroMsg }:SubTitleProps) => {
+const SubTitle = ({ title }:SubTitleProps) => {
     return (
-        <h3 className = "font-bold text-xl p-2 text-center">{heroMsg}</h3>
+        <h3 className = "font-bold text-xl p-2 text-center">{title}</h3>
     )
 };
 
